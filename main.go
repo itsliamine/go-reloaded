086e0916c59400c806135f15fbb7a68106b911b0
Farès Aoudia <fares.aoudia0@gmail.com>
2024-06-04 12:13:44 +0200

    Main file

--- main.go
+++ main.go
@@ -17,7 +17,9 @@ func main() {
 	filecontent := src.FileOpen(args[1])
 	filecontent = src.Converting(filecontent)
 	filecontent = src.Letters(filecontent)
+	filecontent = src.Quotes(filecontent)
+	filecontent = src.Punctuation(filecontent)
+	filecontent = src.Vowels(filecontent)
 
 	fmt.Println(filecontent)
-
 }
